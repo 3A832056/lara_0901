262776c92475f700df1a1249e818110625198ff8
Shen PEI-RU <3a832056@gm.student.ncut.edu.tw>
2021-11-22 22:48:21 +0800

    練習 4：將新增文章儲存的路由

--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,5 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('posts'          , ['as' => 'admin.posts.index' , 'uses' => 'AdminPostsController@index']);
     Route::get('posts/create'   , ['as' => 'admin.posts.create' , 'uses' => 'AdminPostsController@create']);
     Route::get('posts/{id}/edit', ['as' => 'admin.posts.edit'   , 'uses' => 'AdminPostsController@edit']);
+    Route::post('posts', [AdminPostsController::class, 'store'])->name('admin.posts.store');
 });
